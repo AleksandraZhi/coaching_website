e74934e648ba4c5256841e092a5eb000657dddff
Aleksandra Zhi <jiterewa@gmail.com>
2021-09-28 20:59:37 +0100

    Temporary commit to work with file read issue

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,13 +2,24 @@ const path = require('path')
 const fs = require('fs')
 
 const pathToRU = path.join(__dirname, '../public/txt/ru/')
-const fileContents = fs.readFileSync(
-	path.join(pathToRU, 'index/navigation.txt'),
-	'utf-8'
-)
+
+const readMyFile = function (address) {
+	const fileContent = fs.readFileSync(path.join(pathToRU, address), 'utf-8')
+	return fileContent
+}
+
+const navigationFileContents = readMyFile('index/navigation.txt')
+const aboutCoachContents = readMyFile('index/about_coach.txt')
+const possibilitiesContents = readMyFile('index/possibilities.txt')
+const reviewsContents = readMyFile('index/reviews.txt')
 
 exports.RUmainPage = (req, res) => {
-	res.status(200).render('new-look-main', { fileContents })
+	res.status(200).render('new-look-main', {
+		navigationFileContents,
+		aboutCoachContents,
+		possibilitiesContents,
+		reviewsContents,
+	})
 }
 
 exports.RUpaymentsPage = (req, res) => {
